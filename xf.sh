0d8e1f7a98b38e741188107e82e1af19e49a4843
kitty63 <91011598+kitty63@users.noreply.github.com>
2022-04-10 10:31:39 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "2b84cbba-a270-4ca0-a122-d9f51636c418",
                         "alterId": 0
                     }
                 ],
